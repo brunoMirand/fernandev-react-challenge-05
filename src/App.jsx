0dec48ad971b1133a2cc87c70543e35fc392304d
Bruno Miranda <bruno.silva@catho.com>
2023-01-08 15:11:16 -0300

    solve challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 /*
-Consuma a API e liste todos os pokemons da consulta do seguinte endpoint. 
+Consuma a API e liste todos os pokemons da consulta do seguinte endpoint.
 https://pokeapi.co/api/v2/pokemon
 
 Você deve exibir, de cada pokémon:
@@ -21,14 +22,54 @@ EXTRA: se puder ordene por nome.
 */
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon').then((response) => {
+
+      const sortedList = sortAlphabetically(response.data.results);
+      setList(sortedList);
+    });
+  }, []);
 
   return (
     <>
       <h3>desafio fernandev</h3>
       <h1>consumir api pokémon</h1>
+      <hr />
+      {list.map((item) => (<Pokemon key={item.name} data={item} />))}
     </>
   );
+}
+
+const Pokemon = ({ data }) => {
+  const [details, setDetalis] = useState(null);
+
+  useEffect(() => {
+    axios.get(data.url).then(response => setDetalis(response.data));
+  }, []);
+
+  if (!details) {
+    return <div>Carregando...</div>
+  }
+
+  return (
+    <section style={{display: 'flex', alignItems: 'center'}}>
+      <img src={details.sprites.front_default} style={{ width: 80, marginRight: 20}} />
+      <div>
+        <strong>Name:</strong> {details.name} - <strong>Experience:</strong> {details.base_experience}</div>
+    </section>
+  )
+}
+
+
+const sortAlphabetically = (results) => {
+  const sortedList = [...results];
+  sortedList.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  })
+
+  return sortedList;
 }
 
 export default App;
